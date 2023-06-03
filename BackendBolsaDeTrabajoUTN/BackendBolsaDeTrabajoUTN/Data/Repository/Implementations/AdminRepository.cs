a8359c874db371e508c958235d1ee20246fb659e
Lucianosolari <luciano3924@gmail.com>
2023-06-03 10:58:04 -0300

    Borrar usuario y oferta por Admin

--- BackendBolsaDeTrabajoUTN/BackendBolsaDeTrabajoUTN/Data/Repository/Implementations/AdminRepository.cs
+++ BackendBolsaDeTrabajoUTN/BackendBolsaDeTrabajoUTN/Data/Repository/Implementations/AdminRepository.cs
@@ -66,5 +66,31 @@ namespace BackendBolsaDeTrabajoUTN.Data.Repository.Implementations
                 throw new Exception("Conocimiento no encontrado");
             }
         }
+
+        public void DeleteUser(int id)
+        {
+            try
+            {
+                _context.Users.Remove(_context.Users.First(x => x.UserId == id));
+                _context.SaveChanges();
+            }
+            catch
+            {
+                throw new Exception("Usuario no encontrado");
+            }
+        }
+
+        public void DeleteOffer(int id)
+        {
+            try
+            {
+                _context.Offers.Remove(_context.Offers.First(x => x.OfferId == id));
+                _context.SaveChanges();
+            }
+            catch
+            {
+                throw new Exception("Oferta no encontrado");
+            }
+        }
     }
 }
