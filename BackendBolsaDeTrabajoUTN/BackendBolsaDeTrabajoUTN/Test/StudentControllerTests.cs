a71e1b0c85f50ce788000508525a305d954feb7b
Lucianosolari <luciano3924@gmail.com>
2023-07-06 20:58:16 -0300

    Test Ok

--- BackendBolsaDeTrabajoUTN/BackendBolsaDeTrabajoUTN/Test/StudentControllerTests.cs
+++ BackendBolsaDeTrabajoUTN/BackendBolsaDeTrabajoUTN/Test/StudentControllerTests.cs
@@ -1,101 +1,75 @@
-﻿using Xunit;
-using Moq;
-using Microsoft.AspNetCore.Mvc;
-using BackendBolsaDeTrabajoUTN.Controllers;
+﻿using BackendBolsaDeTrabajoUTN.Controllers;
 using BackendBolsaDeTrabajoUTN.Data.Repository.Interfaces;
-using BackendBolsaDeTrabajoUTN.Models;
-using BackendBolsaDeTrabajoUTN.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Security.Claims;
+using Xunit;
 
-namespace BackendBolsaDeTrabajoUTN.Tests
-{ 
-public class StudentControllerTests
+namespace BackendBolsaDeTrabajoUTN.Tests.Controllers
 {
-    [Fact]
-    public void CreateStudent_ValidData_ReturnsCreatedResult()
+    public class StudentControllerTests
     {
-        // Arrange
-        var studentRepositoryMock = new Mock<IStudentRepository>();
-        var companyRepositoryMock = new Mock<ICompanyRepository>();
-        var studentOfferRepositoryMock = new Mock<IStudentOfferRepository>();
-        var studentKnowledgeRepositoryMock = new Mock<IStudentKnowledgeRepository>();
-
-        var controller = new StudentController(
-            studentRepositoryMock.Object,
-            companyRepositoryMock.Object,
-            studentOfferRepositoryMock.Object,
-            studentKnowledgeRepositoryMock.Object);
-
-        var request = new AddStudentRequest
+        [Fact]
+        public void DeleteStudent_ValidId_ReturnsOk()
         {
-            UserName = "john.doe",
-            Password = "P@ssw0rd",
-            ConfirmPassword = "P@ssw0rd",
-            File = 1234,
-            Name = "John",
-            Surname = "Doe",
-            UserEmail = "john.doe@example.com",
-            AltEmail = "john.alt@example.com",
-            DocumentType = "DNI",
-            DocumentNumber = 12345678,
-            CUIL_CUIT = 12345678901234,
-            Birth = new DateTime(1990, 1, 1),
-            Sex = "Male",
-            CivilStatus = "Single"
-        };
+           
+            var studentRepositoryMock = new Mock<IStudentRepository>();
+            var controller = new StudentController(studentRepositoryMock.Object, null, null, null);
 
-        studentRepositoryMock.Setup(repo => repo.GetUsers()).Returns(new List<User>());
-        studentRepositoryMock.Setup(repo => repo.GetStudents()).Returns(new List<Student>());
-        studentRepositoryMock.Setup(repo => repo.CreateStudent(It.IsAny<Student>())).Verifiable();
+            var studentId = 4;
 
-        // Act
-        var result = controller.CreateStudent(request);
+            
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, studentId.ToString())
+            }));
 
-        // Assert
-        var createdResult = Assert.IsType<CreatedResult>(result);
-        Assert.Equal("Estudiante creado", createdResult.Value);
-        studentRepositoryMock.Verify(repo => repo.CreateStudent(It.IsAny<Student>()), Times.Once);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+            
+            var result = controller.DeleteStudent();
+
+            
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Alumno borrado del sistema.", okResult.Value);
+
+            studentRepositoryMock.Verify(repo => repo.RemoveStudent(studentId), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteStudent_Exception_ReturnsProblem()
+        {
+            
+            var studentRepositoryMock = new Mock<IStudentRepository>();
+            studentRepositoryMock.Setup(repo => repo.RemoveStudent(It.IsAny<int>())).Throws(new Exception("Error al eliminar el alumno."));
+            var controller = new StudentController(studentRepositoryMock.Object, null, null, null);
+
+           
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+        new Claim(ClaimTypes.NameIdentifier, "1")
+            }));
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+            
+            var result = controller.DeleteStudent();
+
+            
+            var problemResult = Assert.IsType<ObjectResult>(result);
+            var problemDetails = Assert.IsType<ProblemDetails>(problemResult.Value);
+            Assert.Equal("Error al eliminar el alumno.", problemDetails.Detail);
+
+            studentRepositoryMock.Verify(repo => repo.RemoveStudent(It.IsAny<int>()), Times.Once);
+        }
+
     }
-
-    [Fact]
-    public void CreateStudent_InvalidData_ReturnsBadRequest()
-    {
-        // Arrange
-        var studentRepositoryMock = new Mock<IStudentRepository>();
-        var companyRepositoryMock = new Mock<ICompanyRepository>();
-        var studentOfferRepositoryMock = new Mock<IStudentOfferRepository>();
-        var studentKnowledgeRepositoryMock = new Mock<IStudentKnowledgeRepository>();
-
-        var controller = new StudentController(
-            studentRepositoryMock.Object,
-            companyRepositoryMock.Object,
-            studentOfferRepositoryMock.Object,
-            studentKnowledgeRepositoryMock.Object);
-
-        var request = new AddStudentRequest
-        {
-            // Set the request properties with invalid values for testing
-            UserName = "john.doe",
-            Password = "password",
-            ConfirmPassword = "differentpassword",
-            File = 0,
-            Name = null,
-            Surname = null,
-            UserEmail = "invalidemail",
-            AltEmail = "invalidemail",
-            DocumentType = "InvalidType",
-            DocumentNumber = 0,
-            CUIL_CUIT = 0,
-            Birth = DateTime.Now,
-            Sex = "InvalidSex",
-            CivilStatus = "InvalidStatus"
-        };
-
-        // Act
-        var result = controller.CreateStudent(request);
-
-        // Assert
-        Assert.IsType<BadRequestObjectResult>(result);
-    }    
 }
-}
-
